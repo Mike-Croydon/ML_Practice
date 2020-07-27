0a0f50e3af7bcb13fadde66cdb7297411ffa4a32
Michael Croydon <mikecroydon85@gmail.com>
2020-07-27 16:08:46 -0400

    Compiled, trained, and evaluated the model  keras_first_network.py

--- keras_first_network.py
+++ keras_first_network.py
@@ -18,3 +18,14 @@ model.add(Dense(12, input_dim=8, activation='relu'))
 model.add(Dense(8, activation='relu'))
 model.add(Dense(1, activation='sigmoid'))
 
+# compile the keras model
+#binary_crossentropy is used for binary problems(has or doesn't have diabetes)
+#adam is a stochastic gradient descent algorithm, popular because it tunes itself and is flexible
+model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+
+# fit the keras model on the dataset
+model.fit(X, y, epochs=150, batch_size=10)
+
+# evaluate the keras model
+_, accuracy = model.evaluate(X, y)
+print('Accuracy: %.2f' % (accuracy*100))
