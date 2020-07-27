3764d3f7f7888e6846cc67755544db311aced15e
Michael Croydon <mikecroydon85@gmail.com>
2020-07-27 15:58:41 -0400

    Began building model in keras_first_network.py

--- keras_first_network.py
+++ keras_first_network.py
@@ -8,3 +8,13 @@ dataset = loadtxt('pima-indians-diabetes.csv', delimiter=',')
 # split into input (X) and output (y) variables
 X = dataset[:,0:8]
 y = dataset[:,8]
+
+# define the keras model
+model = Sequential()
+#add layers to our model using the Dense class. The first layer uses input_dim to specify 8 input arguments
+#first two layers use ReLU activation function, final layer uses sigmorid activation function
+#Number of nodes (12,8,1) is first argument of Class constructor
+model.add(Dense(12, input_dim=8, activation='relu'))
+model.add(Dense(8, activation='relu'))
+model.add(Dense(1, activation='sigmoid'))
+
