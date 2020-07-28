2a281c3f03d1a96eb85c3e88f5fdd8be8bf73abb
Michael Croydon <mikecroydon85@gmail.com>
2020-07-27 20:27:50 -0400

    Added catdog_organize.py, which moves photos into useful structure

--- catdog.py
+++ catdog.py
@@ -8,10 +8,12 @@ for i in range(9):
 	# define subplot
 	pyplot.subplot(330 + 1 + i)
 	# define filename
-	filename = folder + 'dog.' + str(i) + '.jpg'
+	filename = folder + 'cat.' + str(i) + '.jpg'
 	# load image pixels
 	image = imread(filename)
 	# plot raw pixel data
 	pyplot.imshow(image)
 # show the figure
 pyplot.show()
+
+
