b7ed619ff4d8c0cd781ad59b252c50a53ebf0d9e
Michael Croydon <mikecroydon85@gmail.com>
2020-07-31 20:34:16 -0400

    Created catdog_organize_final.py to get training data in final structure

---
+++ catdog_organize_final.py
@@ -0,0 +1,23 @@
+# organize dataset into a useful structure
+from os import makedirs
+from os import listdir
+from shutil import copyfile
+from random import seed
+from random import random
+# create directories
+dataset_home = 'finalize_dogs_vs_cats/'
+# create label subdirectories
+labeldirs = ['dogs/', 'cats/']
+for labldir in labeldirs:
+	newdir = dataset_home + labldir
+	makedirs(newdir, exist_ok=True)E
+# copy training dataset images into subdirectories
+src_directory = 'train/'
+for file in listdir(src_directory):
+	src = src_directory + '/' + file
+	if file.startswith('cat'):
+		dst = dataset_home + 'cats/'  + file
+		copyfile(src, dst)
+	elif file.startswith('dog'):
+		dst = dataset_home + 'dogs/'  + file
+		copyfile(src, dst)
